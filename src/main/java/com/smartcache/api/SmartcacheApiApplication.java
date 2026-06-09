befd9f878dc99bb55193badd4435572ee7e2c4dc
Sanjay <sanjayking2526-cloud@gmail.com>
2026-06-10 00:35:19 +0530

    fixed correct package declaration with api

--- src/main/java/com/smartcache/api/SmartcacheApiApplication.java
+++ src/main/java/com/smartcache/api/SmartcacheApiApplication.java
@@ -3,10 +3,13 @@ package com.smartcache.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(excludeName = {"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"})
+@SpringBootApplication
 public class SmartcacheApiApplication {
 
     public static void main(String[] args) {
+        
+        System.setProperty("spring.autoconfigure.exclude", "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration");
+        
         SpringApplication.run(SmartcacheApiApplication.class, args);
     }
 }
