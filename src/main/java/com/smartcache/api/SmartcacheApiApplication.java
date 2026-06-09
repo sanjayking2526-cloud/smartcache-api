422465c4e6c3e4df729a741170f5ff4406e8c2ae
Sanjay <sanjayking2526-cloud@gmail.com>
2026-06-09 16:17:23 +0530

    final configuration issue

--- src/main/java/com/smartcache/api/SmartcacheApiApplication.java
+++ src/main/java/com/smartcache/api/SmartcacheApiApplication.java
@@ -3,11 +3,10 @@ package com.smartcache.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(excludeName = {"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"})
 public class SmartcacheApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartcacheApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SmartcacheApiApplication.class, args);
+    }
 }
